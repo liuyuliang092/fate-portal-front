a6cd75d1ecb0070c63584b6dad1d3f17ed59a309
wuafei <2731253615@qq.com>
2023-02-16 17:18:19 +0800

    恢复节点运行状态边框

--- src/views/butterfly/AlgoDag.vue
+++ src/views/butterfly/AlgoDag.vue
@@ -281,7 +281,7 @@ export default {
   height: 100%;
   background-color: #fff;
   border: 1px solid #c2c8d5;
-  /*border-left: 4px solid #5f95ff;*/
+  border-left: 4px solid #5f95ff;
   border-radius: 12px;
   box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.06);
 }
